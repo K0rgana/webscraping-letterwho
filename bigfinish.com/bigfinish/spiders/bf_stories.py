74b06e119cdf22b484a7a8d3a11e46fe087fb9df
K0rgana <anagrom1999@gmail.com>
2022-12-15 21:36:30 -0300

    fix formating & add more information

--- bigfinish.com/bigfinish/spiders/bf_stories.py
+++ bigfinish.com/bigfinish/spiders/bf_stories.py
@@ -1,37 +1,36 @@
 import scrapy
+from scrapy.crawler import CrawlerProcess
 import logging
 import re
 from datetime import datetime
 
-u_base = 'https://www.bigfinish.com'
-u_range= 'ranges/v/torchwood'
+# See who the url is divided:
+#              |           base         |      range       | page |           filters
+#urlExemple = 'https://www.bigfinish.com/ranges/v/torchwood/page:{}?url=ranges/v/torchwood&sort_ordering=date_asc'
+
+#PASTE INSIDE THE ' ' AND AFTER THE "/v/" THE RANGE NAME/URL FROM BIG FINISH SITE
+u_range= 'ranges/v/torchwood' 
 u_page = "/page:{}"
-#urlPage = 'https://www.bigfinish.com/ranges/v/torchwood/page:{}?url=ranges/v/torchwood&sort_ordering=date_asc'
-urlPage = 'https://www.bigfinish.com/ranges/v/torchwood/page:{}'
+u_base = 'https://www.bigfinish.com'
+u_filter = f"?url={u_range}&sort_ordering=date_asc"
+url = f"{u_base}/{u_range}{u_page.format(1)}{u_filter}"
 
-url = f"{u_base}{u_range}{u_page.format(1)}"
+rg = u_range.split('/')
+rg = rg[2]
 
-class BfSpider(scrapy.Spider):
+class BfStoriesSpider(scrapy.Spider):
     name = 'bf_stories'
-    start_urls = [urlPage.format(1)]
-    #start_urls = [u_base]
-
+    start_urls = [url]
 
     def parse(self, response):
         #get link from items list
         for itn in response.css('.item.release-items'):
             url_item = itn.css('.item.release-items .title a::attr(href)').get()
             url_deep = f"{u_base}{url_item}"
-            #url_deep = 'https://www.bigfinish.com/releases/v/doctor-who-shadow-of-the-daleks-1-2051'
+            #url_deep = 'https://www.bigfinish.com/releases/v/doctor-who-the-war-doctor-only-the-monstrous-1380'
             
             #collect data from each item from list
             yield scrapy.Request(url_deep,callback=self.parse_st)
-
-            """ yield {
-                'title' : itn.css('.item.release-items .title a::text').get().strip(),
-                'url' : urlI,
-                'range' : itn.css('.item.release-items .name a::text').get().strip(),
-            } """
 
             #go to next page
             next_pg = response.css('#pagination a').attrib['href']
@@ -56,7 +55,9 @@ class BfSpider(scrapy.Spider):
             #fix url
             img = st.css('.detail-page-image img::attr(src)').get()
             uri_img = f"{u_base}{img}"
-
+            
+            #trailer url
+            trailer = st.css('#tab4 iframe::attr(src)').get()
 
             CLEANR = re.compile('<.*?>') 
 
@@ -68,42 +69,37 @@ class BfSpider(scrapy.Spider):
             f_cast = []
             f_crew = []
 
+            #clean title and story
+            title_full = st.css('.product-desc h3::text').get().strip()
+            nw_ttl = re.split('\.\s',title_full)
+            story = nw_ttl[0]
+            title = nw_ttl[1]
+
             #clean desc
             desc = st.css('#tab1 article p').getall()
             for ds in desc:
                 a = cleanhtml(ds).strip()
                 f_desc.append(a)
-            
             newlist = "\n"
-            desc = newlist.join(f_desc).replace('\u2019', '\'')
+            desc = newlist.join(f_desc).replace('\u2019', '\'').replace('\u2026', '...')
             
-
-
             #clean duration
             duration = st.xpath('//li[contains( text(), "minute")]/text()').get().strip()
             duration = duration.split(' ')
             duration = duration[0]
 
+            range = st.css('.product-desc h6 a::text').get().strip() 
+            ISBNP = st.xpath('//*[@id="tab6"]/div/div[2]/ul/li[4]/text()').get().strip()
+            ISBND = st.xpath('//*[@id="tab6"]/div/div[2]/ul/li[5]/text()').get().strip()
+            codeProd = st.xpath('//*[@id="tab6"]/div/div[2]/ul/li[6]/text()').get().strip()
+
+            #clear cast
             for cast in st.css('#tab5 li'):
                 name = cast.css('li a::text').get().strip()
                 role = cast.css('li').get().split('\n')
                 role = role[3].replace(')', '').replace('(', '').strip()
                 cs = {'actor' : name,'role' : role }
                 f_cast.append(cs)
-
-            """ for crew in st.css('#tab6'):
-                role = crew.xpath('.//li[@class="comma-seperate-links"]/text()').get().strip()
-                name = crew.xpath('//div[@id="tab6"]//li[1]/a/@title').getall()
-                
-                nm = []
-                if len(name) > 1:
-                    for n in name:
-                        c = {'role' : role ,'p' : n}
-                        nm.append(c)
-                        f_crew.append(nm)
-                else:
-                    cw = {'role' : role ,'pp' : name}
-                    f_crew.append(cw) """
             
             #clear crew
             clear_lines =[]
@@ -113,35 +109,44 @@ class BfSpider(scrapy.Spider):
                 clear_lines.append(line_clear)
 
             for line in clear_lines:
+                line =line.replace('\t', '')
                 line = line.split('\n')
                 role = line[0]
                 line.remove(role)
 
-                #pers = []
                 if len(line) > 1:
                     for newperson in line:
                         c = {'role' : role ,'person' : newperson}
                         f_crew.append(c)
                 else:
-                    pers2 = {'role' : role ,'person' : line}
+                    pers2 = {'role' : role ,'person' : line[0]}
                     f_crew.append(pers2)
 
-            
-                
-                #self.log(pers,logging.WARNING)
-            
             yield {
-                    'story': '',
-                    'title' : st.css('.product-desc h3::text').get().strip(),
-                    'range' : st.css('.product-desc h6 a::text').get().strip(),
+                    'contribution': 'korgana',
+                    'story': story,
+                    'title' : title,
+                    'range' : range,
                     'doctor': '',
                     'airdate' : date,
                     'runtime' : duration,
-
                     'resume' : {'en':desc, 'pt':''},
                     'cast' : f_cast,
                     'crew' : f_crew,
                     'quotes': [{'en':'', 'pt':''},],
+                    'Physical Retail ISBN' : ISBNP,
+                    'Digital Retail ISBN' : ISBND,
+                    'Production Code' : codeProd,
                     'cover' : uri_img,
+                    'trailer' : trailer,
                     'url' : response.url,
                 }
+
+#run the script and make json file
+output = f"output-{rg}.csv"
+process = CrawlerProcess(settings={
+    "FEED_URI": output,
+    "FEED_FORMAT": "csv"
+})
+process.crawl(BfStoriesSpider)
+process.start() # the script will block here until the crawling is finished
